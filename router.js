7e5cb461718503b7ba29b7db4734eefb3ab25368
Dennis Flannigan <DLFlann@gmail.com>
2017-08-25 12:34:17 -0500

    -feat Added functionality to retrieve Treehouse user profile information and return it

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+var Profile = require("./profile.js");
+
 // Handle the HTTP route GET / and POST / i.e. Home
 function home(request, response) {
     // if url === "/" && GET
@@ -18,13 +20,31 @@ function user(request, response) {
     if(username.length > 0) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('Header\n');
-        response.write(username + '\n');
-        response.end('Footer\n');
+
         // get json from Treehouse
+        var studentProfile = new Profile(username);
         // on "end"
+        studentProfile.on("end", function(profileJSON) {
             //show profile
+
+            // Store the values we need
+            const values = {
+                avatarUrl: profileJSON.gravatar_url,
+                username: profileJSON.profile_name,
+                badges: profileJSON.badges.length,
+                javascriptPoints: profileJSON.points.JavaScript
+            };
+            // Simple response
+            response.write(values.username + ' has ' + values.badges + '\n');
+            response.end('Footer\n');
+        });
+
         //on "error"
+        studentProfile.on("error", function(error) {
             //show error
+            response.write(error.message + '\n');
+            response.end('Footer\n');
+        });
     }
 }
 
